bf20a5e24d79a7767b0619fc8ad27d87b7bdff89
globalsmile <mohammedbadmus823@gmail.com>
2023-10-22 15:23:27 -0700

    add: playbook and provision_slave.sh files

---
+++ AltSchool_of_Cloud_Engineering_v2_2nd_Semester_Examination_Project/provision_slave.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Execute master provisioning script on slave
+wget https://raw.githubusercontent.com/globalsmile/altschool-cloud-exercises/master/AltSchool_of_Cloud_Engineering_v2_2nd_Semester_Examination_Project/provision_master.sh
+chmod +x provision_master.sh
+./provision_master.sh
+
+# Verify PHP application accessibility
+# (add necessary commands and take screenshots)
+
+# Create cron job for checking uptime at 12 am
+(crontab -l ; echo "0 0 * * * /usr/bin/uptime >> /var/log/uptime.log") | crontab -
+
