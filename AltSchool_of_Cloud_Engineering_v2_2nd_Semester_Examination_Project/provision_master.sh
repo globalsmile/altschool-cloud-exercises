8a1491db2bbf18deae9584d7f2523aaf8619a831
globalsmile <mohammedbadmus823@gmail.com>
2023-10-22 14:59:26 -0700

    add: provision_master.sh file

---
+++ AltSchool_of_Cloud_Engineering_v2_2nd_Semester_Examination_Project/provision_master.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# SSH into master node
+vagrant up master
+vagrant ssh master
+
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install LAMP stack
+sudo apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql
+
+# Clone PHP application
+git clone https://github.com/laravel/laravel.git /var/www/html/laravel
+
+# Configure Apache
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/laravel.conf
+sudo sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/laravel\/public/' /etc/apache2/sites-available/laravel.conf
+sudo a2ensite laravel
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+# Configure MySQL
+sudo mysql_secure_installation <<EOF
+n
+password
+password
+y
+y
+y
+y
+EOF
+mysql -u root -e "CREATE DATABASE laravel;"
+mysql -u root -e "CREATE USER 'laravel'@'localhost' IDENTIFIED BY 'sample_password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON laravel.* TO 'laravel'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "EXIT;"
